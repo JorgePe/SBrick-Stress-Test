cbfa79d58b6470b8c6e4d2a4a7fbcac721611aba
szigeti@niif.hu <szigeti@niif.hu>
2015-02-12 08:25:17 +0100

    Code refactoring, Tk scale mistake corrections

--- sbstress.py
+++ sbstress.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 from Tkinter import *
 from time import sleep
-from subprocess import call, check_output
+from subprocess import call, check_output, CalledProcessError
 import sys, traceback, os, getopt
 
 # exit codes
@@ -14,80 +14,131 @@ EXIT_HW   = 4
 EXIT_FW   = 5
 EXIT_FWID = 6                                 # Unknown Firmware ID - not safe to proceed
 
-
-BT_ADAPTER = ''
-SBRICK = ''
-SBRICK_HW_VS = ''
-SBRICK_FW_VS = ''
-
-# The SBrick firmware 4.2+ has a watchdog, after 500 ms it drops the BLE connection
-# so we need to keep talking to it
-# the value for PERIOD (in ms) is empirical - you may need to adjust
-PERIOD = 330
-
-
 # This is the Font used to print Temperature and Voltage
 FONT_TYPE = "Helvetica"
 FONT_SIZE = 16
 
-ScaleMAX = 255
-ScaleMIN = 0
+class SBrick:
 
-def twoDigitHex(number):
-    return '%02x' % number
+  BT_ADAPTER = ''
+  SBRICK = ''
+  SBRICK_HW_VS = ''
+  SBRICK_FW_VS = ''
 
-def SBrickGetFw ():
-   return check_output("gatttool --device=" + SBRICK + " --adapter=" + BT_ADAPTER + " --char-read --handle=0x000a", shell=True)
+  # The SBrick firmware 4.2+ has a watchdog, after 500 ms it drops the BLE connection
+  # so we need to keep talking to it
+  # the value for PERIOD (in ms) is empirical - you may need to adjust
+  PERIOD = 330
 
-def SBrickGetHw ():
-  return check_output("gatttool --device=" + SBRICK + " --adapter=" + BT_ADAPTER + " --char-read --handle=0x000c", shell=True)
+  ScaleMAX = 255
+  ScaleMIN = 0
 
-def SBrickDrive (Command):
-  global BT_ADAPTER, SBRICK, SBRICK_FW_VS
+  def __init__(self, adapter, sbrick, period):
+    self.BT_ADAPTER = adapter
+    self.SBRICK = sbrick
+    self.PERIOD = period
 
-  if (SBRICK_FW_VS == "4.0"):
-    call("gatttool --device=" + SBRICK + " --adapter=" + BT_ADAPTER + " --char-write --handle=0x0025 --value=01" + Command, shell=True)
-  elif (SBRICK_FW_VS == "4.2"):
-    call("gatttool --device=" + SBRICK + " --adapter=" + BT_ADAPTER + " --char-write --handle=0x001a --value=01" + Command, shell=True)
-  return 0
+    print(' Adapter:    ', self.BT_ADAPTER)
+    print(' Device:     ', self.SBRICK)
+    print(' Period:     ', self.PERIOD)
+
+    # find SBrick hardware and firmware version
+    # should return something like 'Characteristic value/descriptor: 34 2e 30'
+    # 34 = '4'  2E =' .'  30 = '0'
+    # can have more chars for minor versions like 4.2b2 but will use just the first 3
+
+    try:
+      result=self.GetHw()
+      parsed_result=result.split(" ")
+      self.SBRICK_HW_VS=(parsed_result[2]+parsed_result[3]+parsed_result[4]).decode("hex")
+    except CalledProcessError:
+      print("Could not read SBrick hardware version")
+      exit(EXIT_HW)
+
+    try:
+      result=self.GetFw()
+      parsed_result=result.split(" ")
+      self.SBRICK_FW_VS=(parsed_result[2]+parsed_result[3]+parsed_result[4]).decode("hex")
+    except CalledProcessError:
+      print("Could not read SBrick firmware version")
+      exit(EXIT_FW)
+
+    print(' SBrick Hw:   ' + self.SBRICK_HW_VS)
+    print(' SBrick Fw:   ' + self.SBRICK_FW_VS)
+
+    if(self.SBRICK_FW_VS == "4.0"):
+      print("Will use SBrick firmware 4.0 handles")
+      print("Will limit values to 0..FE as FF doesn't work")
+      self.ScaleMAX = 254
+    elif(self.SBRICK_FW_VS == "4.2"):
+      print("Will use SBrick firmware 4.2 handles")
+      self.ScaleMAX = 255
+    else:
+      print("Don't know how to handle this firmware version")
+      sys.exit(EXIT_FWID)
 
 
-def SBrickReadTemp():
+  def GetFw (self):
+     return check_output("gatttool --device=" + self.SBRICK + " --adapter=" + self.BT_ADAPTER + " --char-read --handle=0x000A", shell=True)
 
-  global BT_ADAPTER, SBRICK, SBRICK_FW_VS
+  def GetHw (self):
+     return check_output("gatttool --device=" + self.SBRICK + " --adapter=" + self.BT_ADAPTER + " --char-read --handle=0x000C", shell=True)
 
-  if(SBRICK_FW_VS=="4.2"):
-    call("gatttool -b "+SBRICK+" -i "+BT_ADAPTER+" --char-write --handle=0x001A --value=0F0E",shell=True)
-    sleep(0.01)
-    result=check_output("gatttool -b "+SBRICK+" -i "+BT_ADAPTER+" --char-read --handle=0x001A",shell=True).split(" ")
-    t=( int(result[3]+result[2], 16) ) * 0.008413396 - 160
+  def GetScaleMAX (self):
+     return self.ScaleMAX
 
-    return("{0:.1f}".format(t)+" C")
-  else:
-    # firmware 4.0 can't read temp/volt
-    return("00.0 C")
+  def GetScaleMIN (self):
+     return self.ScaleMIN
 
-def SBrickReadVolt():
+  def GetPeriod (self):
+     return self.PERIOD
 
-  global BT_ADAPTER, SBRICK, SBRICK_FW_VS
+  def Drive (self, Command):
+    if (self.SBRICK_FW_VS == "4.0"):
+      call("gatttool --device=" + self.SBRICK + " --adapter=" + self.BT_ADAPTER + " --char-write --handle=0x0025 --value=01" + Command, shell=True)
+    elif (self.SBRICK_FW_VS == "4.2"):
+      call("gatttool --device=" + self.SBRICK + " --adapter=" + self.BT_ADAPTER + " --char-write --handle=0x001A --value=01" + Command, shell=True)
+    return 0
 
-  if(SBRICK_FW_VS=="4.2"):
-    call("gatttool -b "+SBRICK+" -i "+BT_ADAPTER+" --char-write --handle=0x001A --value=0F00",shell=True)
-    sleep(0.01)
-    result=check_output("gatttool -b "+SBRICK+" -i "+BT_ADAPTER+" --char-read --handle=0x001A",shell=True).split(" ")
-    v=( int(result[3]+result[2], 16) ) * 0.000378603  
 
-    return("{0:.1f}".format(v)+" V")
-  else:
-    # firmware 4.0 can't read temp/volt
-    return("0.0 V")
+  def ReadTemp(self):
+    if(self.SBRICK_FW_VS=="4.2"):
+      call("gatttool -b "+self.SBRICK+" -i "+self.BT_ADAPTER+" --char-write --handle=0x001A --value=0F0E",shell=True)
+      sleep(0.01)
+      result=check_output("gatttool -b "+self.SBRICK+" -i "+self.BT_ADAPTER+" --char-read --handle=0x001A",shell=True).split(" ")
+      t=( int(result[3]+result[2], 16) ) * 0.008413396 - 160
+
+      return("{0:.1f}".format(t)+" C")
+    else:
+      # firmware 4.0 can't read temp/volt
+      return("00.0 C")
+
+  def ReadVolt(self):
+    if(self.SBRICK_FW_VS=="4.2"):
+      call("gatttool -b "+self.SBRICK+" -i "+self.BT_ADAPTER+" --char-write --handle=0x001A --value=0F00",shell=True)
+      sleep(0.01)
+      result=check_output("gatttool -b "+self.SBRICK+" -i "+self.BT_ADAPTER+" --char-read --handle=0x001A",shell=True).split(" ")
+      v=( int(result[3]+result[2], 16) ) * 0.000378603  
+
+      return("{0:.1f}".format(v)+" V")
+    else:
+      # firmware 4.0 can't read temp/volt
+      return("0.0 V")
 
 
 class Tool:
 
-  global ScaleMAX,ScaleMIN
+  ScaleMAX = 0
+  ScaleMIN = 0
+  LENGTH = 0
+  SBRICK = ''
 
-  def __init__(self):
+  def __init__(self, sbrick):
+    self.SBRICK = sbrick
+    self.ScaleMAX = sbrick.GetScaleMAX()
+    self.ScaleMIN = sbrick.GetScaleMIN()
+    self.LENGTH = abs(self.ScaleMAX - self.ScaleMIN)
+
     self.root=Tk()
     self.root.title("SBrick Stress Test")
 
@@ -123,25 +174,25 @@ class Tool:
     CheckPort1=Checkbutton(self.root, variable = self.check1,takefocus=1, text="Port #1", padx=50, pady=10)
     CheckPort1.grid(row=1,column=0)
 
-    Port1 = Scale(self.root, from_=ScaleMAX, to=ScaleMIN, digits=3, resolution=5, orient=VERTICAL, length=255, takefocus=1, command=self.Sync, variable=self.pwm1)
+    Port1 = Scale(self.root, from_=self.ScaleMAX, to=self.ScaleMIN, digits=3, resolution=1, orient=VERTICAL, length=self.LENGTH, takefocus=1, command=self.Sync, variable=self.pwm1)
     Port1.grid(row=2,column=0)
 
     CheckPort2=Checkbutton(self.root, variable = self.check2,takefocus=1, text="Port #2", padx=50, pady=10)
     CheckPort2.grid(row=1,column=1)
 
-    Port2 = Scale(self.root, from_=255, to=0, digits=3, resolution=5, orient=VERTICAL, length=255,  takefocus=1, command=self.Sync, variable=self.pwm2)
+    Port2 = Scale(self.root, from_=self.ScaleMAX, to=self.ScaleMIN, digits=3, resolution=1, orient=VERTICAL, length=self.LENGTH,  takefocus=1, command=self.Sync, variable=self.pwm2)
     Port2.grid(row=2,column=1)
 
     CheckPort3=Checkbutton(self.root, variable = self.check3,takefocus=1, text="Port #3", padx=50, pady=10)
     CheckPort3.grid(row=1,column=2)
 
-    Port3 = Scale(self.root, from_=255, to=0, digits=3, resolution=5, orient=VERTICAL, length=255,  takefocus=1, command=self.Sync, variable=self.pwm3)
+    Port3 = Scale(self.root, from_=self.ScaleMAX, to=self.ScaleMIN, digits=3, resolution=1, orient=VERTICAL, length=self.LENGTH,  takefocus=1, command=self.Sync, variable=self.pwm3)
     Port3.grid(row=2,column=2)
 
     CheckPort4=Checkbutton(self.root, variable = self.check4,takefocus=1, text="Port #4", padx=50, pady=10)
     CheckPort4.grid(row=1,column=3)
 
-    Port4 = Scale(self.root, from_=255, to=0, digits=3, resolution=5, orient=VERTICAL, length=255, takefocus=1, command=self.Sync, variable=self.pwm4)
+    Port4 = Scale(self.root, from_=self.ScaleMAX, to=self.ScaleMIN, digits=3, resolution=1, orient=VERTICAL, length=self.LENGTH, takefocus=1, command=self.Sync, variable=self.pwm4)
     Port4.grid(row=2,column=3)
 
     LabelQuit=Label(self.root, height=1, pady=10)
@@ -149,26 +200,29 @@ class Tool:
     Button_QUIT = Button(text = "QUIT", command = self.quit)
     Button_QUIT.grid(row=4, column=1, columnspan=2)
 
+  def twoDigitHex(self,number):
+      return '%02x' % number
+
   def Sync(self, *ignore):
     if(self.check1.get()==0):
-      SBrickDrive("0000"+twoDigitHex(self.pwm1.get()))
+      self.SBRICK.Drive("0000"+self.twoDigitHex(self.pwm1.get()))
     else:
-      SBrickDrive("0001"+twoDigitHex(self.pwm1.get()))
+      self.SBRICK.Drive("0001"+self.twoDigitHex(self.pwm1.get()))
 
     if(self.check2.get()==0):
-      SBrickDrive("0100"+twoDigitHex(self.pwm2.get()))
+      self.SBRICK.Drive("0100"+self.twoDigitHex(self.pwm2.get()))
     else:
-      SBrickDrive("0101"+twoDigitHex(self.pwm2.get()))
+      self.SBRICK.Drive("0101"+self.twoDigitHex(self.pwm2.get()))
 
     if(self.check3.get()==0):
-      SBrickDrive("0200"+twoDigitHex(self.pwm3.get()))
+      self.SBRICK.Drive("0200"+self.twoDigitHex(self.pwm3.get()))
     else:
-      SBrickDrive("0201"+twoDigitHex(self.pwm3.get()))
+      self.SBRICK.Drive("0201"+self.twoDigitHex(self.pwm3.get()))
 
     if(self.check4.get()==0):
-      SBrickDrive("0300"+twoDigitHex(self.pwm4.get()))
+      self.SBRICK.Drive("0300"+self.twoDigitHex(self.pwm4.get()))
     else:
-      SBrickDrive("0301"+twoDigitHex(self.pwm4.get()))
+      self.SBRICK.Drive("0301"+self.twoDigitHex(self.pwm4.get()))
 
     return
                
@@ -179,9 +233,9 @@ class Tool:
 
   def refresh(self):
 
-    self.temp.set(SBrickReadTemp())
-    self.volt.set(SBrickReadVolt()) 
-    self.root.after(PERIOD,self.refresh)  # reschedule event
+    self.temp.set(self.SBRICK.ReadTemp())
+    self.volt.set(self.SBRICK.ReadVolt()) 
+    self.root.after(self.SBRICK.GetPeriod(),self.refresh)  # reschedule event
     self.Sync()
 
 def print_help():
@@ -197,13 +251,10 @@ def print_help():
 
 def print_version():
   print('')
-  print('SBrick Stress Test Tool 0.3 - Jorge Pereira - February 2015')
+  print('SBrick Stress Test Tool 0.4 - Jorge Pereira - February 2015')
 
 
 def main(argv):
-
-  global BT_ADAPTER, SBRICK, SBRICK_FW_VS
-  global ScaleMAX,ScaleMIN
 
   try:
     opts, args = getopt.getopt(argv,"hva:d:p:")
@@ -223,58 +274,19 @@ def main(argv):
       elif opt == '-a':
         BT_ADAPTER = arg
       elif opt == '-d':
-        SBRICK = arg
+        SBRICK_ADDR = arg
       elif opt == '-p':
         PERIOD = arg
 
-    if( BT_ADAPTER=='')or(SBRICK=='')or(PERIOD==''):
+    if( BT_ADAPTER=='')or(SBRICK_ADDR=='')or(PERIOD==''):
       print_help()
       sys.exit(EXIT_ARGS)
     else:
       print('')
       print('sbstress - SBrick Stress Test Tool')
-      print(' Adapter:    ', BT_ADAPTER)
-      print(' Device:     ', SBRICK)
-      print(' Period:     ', PERIOD)
+      SBRICK = SBrick(BT_ADAPTER, SBRICK_ADDR, PERIOD)
 
-    # find SBrick hardware and firmware version
-    # should return something like 'Characteristic value/descriptor: 34 2e 30'
-    # 34 = '4'  2E =' .'  30 = '0'
-    # can have more chars for minor versions like 4.2b2 but will use just the first 3
-
-    try:
-      result=SBrickGetHw()
-      parsed_result=result.split(" ")
-      SBRICK_HW_VS=(parsed_result[2]+parsed_result[3]+parsed_result[4]).decode("hex")
-    except CalledProcessError:
-      print("Could not read SBrick hardware version")
-      exit(EXIT_HW)
-
-    try:
-      result=SBrickGetFw()
-      parsed_result=result.split(" ")
-      SBRICK_FW_VS=(parsed_result[2]+parsed_result[3]+parsed_result[4]).decode("hex")
-    except CalledProcessError:
-      print("Could not read SBrick firmware version")
-      exit(EXIT_FW)
-
-    print(' SBrick Hw:   ' + SBRICK_HW_VS)
-    print(' SBrick Fw:   ' + SBRICK_FW_VS)
-
-    if(SBRICK_FW_VS == "4.0"):
-      print("Will use SBrick firmware 4.0 handles")
-      print("Will limit values to 0..FE as FF doesn't work")
-      ScaleMAX = 254
-      ScaleMIN = 0
-    elif(SBRICK_FW_VS == "4.2"):
-      print("Will use SBrick firmware 4.2 handles")
-      ScaleMAX = 255
-      ScaleMIN = 0
-    else:
-      print("Don't know how to handle this firmware version")
-      sys.exit(EXIT_FWID)
-
-    tool = Tool()
+    tool = Tool(SBRICK)
     tool.root.after(PERIOD,tool.refresh)
     tool.root.mainloop()
 
